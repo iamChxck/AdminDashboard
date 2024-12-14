06ae3e369e88c2de8afab59b5971a1e28e33ceb7
iamChxck <55932456+iamChxck@users.noreply.github.com>
2024-12-14 04:31:25 +0400

    Finished Sales Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 
+import Sidebar from "./components/Sidebar";
+
 import OverviewPage from "./pages/OverviewPage";
 import ProductsPage from "./pages/ProductsPage";
-
-import Sidebar from "./components/Sidebar";
 import UsersPage from "./pages/UsersPage";
+import SalesPage from "./pages/SalesPage";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path='/' element={<OverviewPage />} />
         <Route path='/products' element={<ProductsPage />} />
         <Route path='/users' element={<UsersPage />} />
+        <Route path='/sales' element={<SalesPage />} />
       </Routes>
     </div>
   )
---
+++ src/pages/SalesPage.jsx
@@ -0,0 +1,59 @@
+import { motion } from "framer-motion";
+
+import Header from "../components/common/Header";
+import StatCard from "../components/common/StatCard";
+import { CreditCard, DollarSign, ShoppingCart, TrendingUp } from "lucide-react";
+import SalesOverviewChart from "../components/Sales/SalesOverviewChart";
+import SalesByCategoryChart from "../components/Sales/SalesByCategoryChart";
+import DailySalesTrend from "../components/Sales/DailySalesTrend";
+
+const SALES_STATS_DATA = {
+    totalRevenue: "$1,234,567",
+    averageOrderValue: "$78.90",
+    conversionRate: "3.45%",
+    salesGrowth: "12.3%",
+};
+
+const SalesPage = () => {
+    return (
+        <div className="flex-1 overflow-auto relative z-10">
+            <Header title="Sales Dashboard" />
+
+            <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
+
+                {/* SALES STATS */}
+                <motion.div
+                    className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 1 }}
+                >
+                <StatCard name='Total Revenue' icon={DollarSign} value={SALES_STATS_DATA.totalRevenue} color='#6366F1' />
+					<StatCard
+						name='Avg. Order Value'
+						icon={ShoppingCart}
+						value={SALES_STATS_DATA.averageOrderValue}
+						color='#10B981'
+					/>
+					<StatCard
+						name='Conversion Rate'
+						icon={TrendingUp}
+						value={SALES_STATS_DATA.conversionRate}
+						color='#F59E0B'
+					/>
+					<StatCard name='Sales Growth' icon={CreditCard} value={SALES_STATS_DATA.salesGrowth} color='#EF4444' />
+				</motion.div>
+
+                {/* AREA CHART */}
+                <SalesOverviewChart />
+
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 mb-8">
+                    <SalesByCategoryChart />
+                    <DailySalesTrend />
+                </div>
+            </main>
+        </div>
+    )
+};
+
+export default SalesPage;
